4002376d26c10e66c04ae2d5a0c42f534da552c7
mmanov <mmanov@adsterra.com>
2024-08-21 16:32:24 +0300

    tests

--- src/Cli.php
+++ src/Cli.php
@@ -6,7 +6,7 @@ use function cli\line;
 use function cli\prompt;
 
 function run()
-{   
+{
     line('Welcome to the Brain Games!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
