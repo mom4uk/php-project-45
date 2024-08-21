864981ae925745f0de69cbb9304f6fa03a45522c
mmanov <mmanov@adsterra.com>
2024-08-21 15:14:20 +0300

    tests

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -6,18 +6,7 @@ use function cli\line;
 use function cli\prompt;
 use function Engine\play;
 
-function getTaskGcd(int $firstNum, int $secondNum)
+function getTask(int $firstNum, int $secondNum)
 {
     return "{$firstNum} {$secondNum}";
-}
-
-function startGcd()
-{
-    line('Welcome to the brain game!');
-    $name = prompt('May i have your name?');
-    line("hello, %s!", $name);
-
-    $question = 'Find the greatest common divisor of given numbers.';
-
-    play('gcd', $question, $name);
 }
